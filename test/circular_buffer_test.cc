d4f4ac70025d1445117f7c75afef9eaa4372b062
Liu Jiaming <jiaming@qcraft.ai>
2021-07-14 11:28:12 +0800

    Refined circular_buffer_test

--- test/circular_buffer_test.cc
+++ test/circular_buffer_test.cc
@@ -1,13 +1,36 @@
-#include <boost/circular_buffer.hpp>
+#include "boost/circular_buffer.hpp"
 
-int main()
-{
-  boost::circular_buffer<int> cb(1);
+#include <cstdlib>
+#include <iostream>
+
+#define EXPECT_EQ(actual, expected)                                  \
+  do {                                                               \
+    if ((actual) != (expected)) {                                    \
+      std::cerr << "Expected=" << (expected) << ", Got=" << (actual) \
+                << std::endl;                                        \
+      exit(1);                                                       \
+    }                                                                \
+  } while (0)
+
+int main() {
+  constexpr int kSize = 3;
+  boost::circular_buffer<int> cb(kSize);
+  EXPECT_EQ(cb.capacity(), kSize);
   cb.push_back(1);
-
-  if (cb[0] != 1) {
-    return 1;
-  }
-
+  EXPECT_EQ(cb.size(), 1);
+  cb.push_back(2);
+  cb.push_back(3);
+  EXPECT_EQ(cb.size(), kSize);
+  cb.push_back(4);
+  cb.push_back(5);
+  EXPECT_EQ(cb.size(), kSize);
+  EXPECT_EQ(cb.capacity(), kSize);
+  EXPECT_EQ(cb[0], 3);
+  EXPECT_EQ(cb[2], 5);
+  cb.pop_back();
+  EXPECT_EQ(cb.size(), kSize - 1);
+  cb.pop_front();
+  EXPECT_EQ(cb.size(), kSize - 2);
+  EXPECT_EQ(cb[0], 4);
   return 0;
 }
